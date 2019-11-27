8ee6dc25dab5561d381e4c74bfab2efa00ea1ab3
ChristianNguyen <christiannguyen.wk@gmail.com>
2019-11-26 20:40:43 -0500

    Initial Commit: First app - Iam Rich

---
+++ IamRich/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Christian Nguyen on 2019-11-26.
+//  Copyright © 2019 Christian McAwesome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
