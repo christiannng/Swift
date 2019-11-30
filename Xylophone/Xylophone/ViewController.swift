f9db4f3732905f6294aa754698645eff3d365806
ChristianNguyen <christiannguyen.wk@gmail.com>
2019-11-30 16:31:26 -0500

    [Christian] Created 1 function, which handles multiple buttons

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        let soundName = sender.currentTitle
+        
+        playSound(soundName: soundName!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
