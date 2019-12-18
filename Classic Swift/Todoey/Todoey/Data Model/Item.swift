aae3decd99ff763769dc3b6b8b1349b425c3629b
ChristianNguyen <christiannguyen.wk@gmail.com>
2019-12-18 13:22:15 -0500

    [Christian] Work with Encodable to save data locally.

---
+++ Classic Swift/Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Christian Nguyen on 2019-12-18.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var isDone: Bool = false
+}
