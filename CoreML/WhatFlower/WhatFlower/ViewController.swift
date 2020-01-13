66993bfcbe4be7cfac271990bfb6ce4f06b73291
ChristianNguyen <christiannguyen.wk@gmail.com>
2020-01-13 07:25:22 -0500

    [Christian] Made an app using FlowerDetectML and Wiki Api

---
+++ CoreML/WhatFlower/WhatFlower/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Christian Nguyen on 2020-01-12.
+//  Copyright © 2020 Christian McAwesome. All rights reserved.
+//
+
+import UIKit
+import Vision
+import CoreML
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    let imagePicker = UIImagePickerController()
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
+    
+    var userPickedImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let takenImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            guard let convertedImage = CIImage(image: takenImage) else {
+                fatalError("Cannot convert to CIImage")
+            }
+            
+            detect(image: convertedImage)
+            userPickedImage = takenImage
+//            imageView.image = userPickedImage
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify the image.")
+            }
+            
+            self.navigationItem.title = classification.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String) {
+        
+        let parameters : [String: String] = [
+            "format": "json",
+            "action": "query",
+            "prop": "extracts|pageimages",
+            "exintro": "",
+            "explaintext": "",
+            "titles": flowerName,
+            "indexpageids": "",
+            "redirects": "1",
+            "pithumbsize": "500"
+        ]
+        
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                print("Got the wiki page")
+                print(response)
+                
+                let flowerJSON: JSON = JSON(response.result.value!)
+                let pageID = flowerJSON["query"]["pageids"][0].stringValue
+                let flowerDescription = flowerJSON["query"]["pages"][pageID]["extract"].stringValue
+                let flowerImageURL = flowerJSON["query"]["pages"][pageID]["thumbnail"]["source"].stringValue
+                
+                if flowerImageURL != "" {
+                    self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                } else {
+                    self.imageView.image = self.userPickedImage
+                }
+                
+                self.label.text = flowerDescription
+                
+            }
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
